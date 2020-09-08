e8ebe5193611c38809f7799cc487ebe5e24a922a
tj-kohei <68324989+tj-kohei@users.noreply.github.com>
2020-09-09 03:10:51 +0900

    profile.rb

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,21 @@
 class Profile < ApplicationRecord
+  belongs_to :user
+  mount_uploader :avatar, AvatarUploader
+
+  def nickname_to_username
+    if nickname.blank?
+      "@" + user.nickname_to_username
+    else
+      nickname
+    end
+end
+
+def usernmame?
+  if nickname.present?
+    "@" + user.usernmame
+  else
+
+  end
+end
+
 end
