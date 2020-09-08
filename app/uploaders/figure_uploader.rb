b7c0e269e0d779de0fc416ab076d725c0ba7ab03
tj-kohei <68324989+tj-kohei@users.noreply.github.com>
2020-09-09 03:55:57 +0900

    application controller

--- app/uploaders/figure_uploader.rb
+++ app/uploaders/figure_uploader.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
 
-class AvatarUploader < CarrierWave::Uploader::Base
+class FigureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-   include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -33,22 +33,18 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [75, 75]
+    process :resize_to_fit => [668, 300]
   end
 
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
   def extension_white_list
     %w(jpg jpeg gif png)
   end
 
   def default_url
-    "default_avatar.png"
+    "default_article.png"
   end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
