40aaf09b3f6947821d8a636bebee52714dcb580b
tj-kohei <68324989+tj-kohei@users.noreply.github.com>
2020-09-11 13:30:00 +0900

    READMEの修正

--- db/migrate/20200908090639_create_comments.rb
+++ db/migrate/20200908090639_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.timestamps 
+      t.string :content
+      t.timestamps null: false
+      
     end
   end
 end
