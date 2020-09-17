170d20068066fbe378dc5a07c8e2cda8672f32d9
tj-kohei <68324989+tj-kohei@users.noreply.github.com>
2020-09-17 16:27:40 +0900

    ビューの追加及び修正

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,2 +1,11 @@
 module IdeasHelper
+  def add_glyphicon_to_category(idea)
+    if idea.category == "web"
+      '<span class="glyphicon glyphicon-globe">' + idea.category_i18n + '</span>'
+    elsif idea.category == "mobile"
+      '<span class="glyphicon glyphicon-phone">' + idea.category_i18n + '</span>'
+    elsif idea.category == "desk_top"
+      '<span class="glyphicon glyphicon-home">' + idea.category_i18n + '</span>'
+    end
+  end
 end
